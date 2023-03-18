6f77b42896836ec287331b71787e1b804d6dc29b
Joel-Joshi_2002 <92676238+JoelJoshi2002@users.noreply.github.com>
2023-03-19 03:06:06 +0530

    Entry page lost corected

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Login from "./components/Pages/Login/Login";
-import SignUp from "./components/Pages/SignUp/SignUp";
+// import Login from "./components/Pages/Login/Login";
+// import SignUp from "./components/Pages/SignUp/SignUp";
 import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
 import Header from "./components/Header/Header.jsx";
 import Row from "react-bootstrap/esm/Row";
@@ -11,7 +11,15 @@ import Entry from "./components/Pages/Entry/Entry";
 function App() {
   return (
     <>
-      <Login />
+      <Header />
+      <Row>
+        <Col lg={2}>
+          <NavigationBar />
+        </Col>
+        <Col ld={12}>
+          <Entry />
+        </Col>
+      </Row>
     </>
   );
 }
