4bc0961bedcd2474ca91e60889d4ac383e40adce
al-312 <albinr312@gmail.com>
2023-03-19 12:37:13 +0530

    Router changed

--- src/components/router/router.jsx
+++ src/components/router/router.jsx
@@ -3,14 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import Entry from "../Pages/Entry/Entry";
 import History from "../Pages/History/History";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 function Router() {
-  return (
+  const login = () => {
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Signup" element={<SignUp />} />
+    </Routes>;
+  };
+
+  const others = () => {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/Entry" element={<Entry />} />
       <Route path="/History" element={<History />} />
-    </Routes>
-  );
+    </Routes>;
+  };
 }
 
 export default Router;
