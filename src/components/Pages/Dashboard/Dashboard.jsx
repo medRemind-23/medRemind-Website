af8195ea10fc17222d6b506d77378c6d97c16e16
Joel-Joshi_2002 <92676238+JoelJoshi2002@users.noreply.github.com>
2023-03-19 09:09:05 +0530

    Dashboard updated

--- src/components/Pages/Dashboard/Dashboard.jsx
+++ src/components/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./Dashboard.css";
+import { FaUser } from "react-icons/fa";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body
+              className="DashBoard-Card"
+              style={{ backgroundColor: "#329DFF" }}
+            >
+              <h1>
+                <FaUser />
+              </h1>
+              <h2>Total Patients:</h2>
+              <h2>--</h2>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body
+              className="DashBoard-Card"
+              style={{ backgroundColor: "#1EBFC4" }}
+            >
+              <h1>
+                <FaUser />
+              </h1>
+              <h2>Total Entry:</h2>
+              <h2>--</h2>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body
+              className="DashBoard-Card"
+              style={{ backgroundColor: "#3DC86C" }}
+            >
+              <h1>
+                <FaUser />
+              </h1>
+              <h2>Total Hearts Earned: </h2>
+              <h2>--</h2>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Dashboard;
